7b32cf3cb7a71e56a91ef298de94b82df420837b
kxrai <alicia.2005.loi@gmail.com>
2024-11-10 18:09:34 -0500

    fixes navbar import statement in homepage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import RecentSplits from '../components/RecentSplits';
-import Navbar from '../components/Navbar';
+import Navbar from './components/Navbar';
 
 function Homepage() {
   const recentSplitsData = [
