9ccbbb0918cea89281bb6db4bc9d3826a12d71e2
jabineus <mollah25@my.yorku.ca>
2024-11-17 14:19:37 -0800

    New server directory for Express

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/api', (req, res) => {
+  res.json({ message: "Hello from the backend!" });
+});
+
+// Catch-all route for React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
