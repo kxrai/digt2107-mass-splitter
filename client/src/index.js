98551be0b8d85312bc3a6558945fe288e439daf6
Alicia <116113716+kxrai@users.noreply.github.com>
2025-03-25 14:03:07 -0400

    refactor: load Google client ID from .env file using REACT_APP_ prefix

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,32 @@
-// src/index.js
+// // src/index.js
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css'; // Import Tailwind CSS styles
+// import App from './App';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//   <React.StrictMode>
+//     <GoogleOAuthProvider clientId="584206264281-4udrb623en2sg85nq3b58eevjm8elf0r.apps.googleusercontent.com">
+//     <App />
+//     </GoogleOAuthProvider>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'; // Import Tailwind CSS styles
-import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
-root.render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId="584206264281-4udrb623en2sg85nq3b58eevjm8elf0r.apps.googleusercontent.com">
-    <App />
+function Root() {
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>
-);
+  );
+}
+
+export default Root;
